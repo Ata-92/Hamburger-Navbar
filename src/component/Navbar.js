02b7588631dd1f70de8a1562c743d72e4450c30c
Ata-92 <atakal92@gmail.com>
2021-05-11 23:50:56 +0300

    Add styling of Navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <div className="Navbar">
       <button type="button">
-        {String(`<ata />`)}
-        design
+        {String(`<ata/> `)}
+        <span>design</span>
       </button>
       <nav>
         <a href="works">Works</a>
@@ -13,7 +13,7 @@ const Navbar = () => {
         <a href="career">Career</a>
         <a href="contact">Contact</a>
       </nav>
-      <i class="fas fa-bars"></i>
+      {/* <i class="fas fa-bars"></i> */}
     </div>
   );
 };
