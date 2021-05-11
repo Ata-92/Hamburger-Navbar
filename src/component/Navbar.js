99161bc1ed1cc3a002a144f4eb438cca36549ec1
Ata-92 <atakal92@gmail.com>
2021-05-11 23:13:01 +0300

    Add the navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,19 @@
 
 
 const Navbar = () => {
-
   return (
     <div className="Navbar">
-
+      <button type="button">
+        {String(`<ata />`)}
+        design
+      </button>
+      <nav>
+        <a href="works">Works</a>
+        <a href="about">About</a>
+        <a href="career">Career</a>
+        <a href="contact">Contact</a>
+      </nav>
+      <i class="fas fa-bars"></i>
     </div>
   );
 };
